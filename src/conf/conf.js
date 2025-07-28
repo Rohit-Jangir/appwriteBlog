62414d62bfd61ed02fcb802e22a5a9e4b24ee857
RohitJangir <jangirrohit553@gmail.com>
2025-07-28 11:49:59 +0530

    conf

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -7,7 +7,9 @@ const conf = {
 }
 
 console.log("conff", import.meta.env.VITE_APPWRITE_URL);
+console.log("p-id", import.meta.env.VITE_APPWRITE_PROJECT_ID);
 console.log("b-id", import.meta.env.VITE_APPWRITE_BUCKET_ID);
+console.log("c-id", import.meta.env.VITE_APPWRITE_COLLECTION_ID);
 console.log("db-id", import.meta.env.VITE_APPWRITE_DATABASE_ID);
 
 // there was a name issue with the import.meta.env.VITE_APPWRITE_URL, it was later fixed in debugging video
